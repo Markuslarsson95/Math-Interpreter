fd534147765f1db2618cbc37759c6337d59f7b3a
marku <marku@DESKTOP-A63OUJ5>
2024-09-03 22:12:16 +0200

    Add project files.

---
+++ Math Interpreter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main()
+    {
+        //string expression = "1 + (36 / 2) / (3 * 3) - 2";
+        string expression = "20 + 20 - 5 - 5 + 5";
+        string result = EvaluateExpression(expression);
+        Console.WriteLine($"Epression: {expression}");
+        Console.WriteLine($"Result: {result}");
+        Console.ReadLine();        
+    }
+    public static string EvaluateExpression(string expression)
+    {
+        // Hämta nummren från strängen
+        var numbers = Regex.Split(expression, @"[\+\-]")
+            .Select(Convert.ToDouble)
+            .ToList();
+
+        // Hämta symboler från strängen
+        var symbols = Regex.Matches(expression, @"[\+\-]")
+            .Cast<Match>()
+            .Select(m => m.Value[0])
+            .ToList();
+
+        // Tilldelar först numret till resultatet
+        double result = numbers[0];
+
+        // Gå igenom alla symboler
+        for (int i = 0; i < symbols.Count; i++)
+        {
+            // Bestäm vilken operation som ska genomföras
+            char operation = symbols[i];
+
+            // Få näst kommande nummer
+            double nextNumber = numbers[i + 1];
+
+            if (operation == '+')
+            {
+                result = CalculateAddition(result, nextNumber);
+            }
+            else if (operation == '-')
+            {
+                result = CalculateSubtraction(result, nextNumber);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    public static double CalculateAddition(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public static double CalculateSubtraction(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+}
